ab44460754011d3accc16af5802945f63b37ab38
Deepender Kumar <deep@Deependers-MacBook-Air.local>
2026-08-08 09:44:23 +0530

    add question and solution for frontback

--- ques.java
+++ ques.java
@@ -3,3 +3,18 @@ public class Solution {
         System.out.println("CodingBat Solution");
     }
 }
+/*Given a string, return a new string where the first and last chars have been exchanged.
+
+
+frontBack("code") → "eodc"
+frontBack("a") → "a"
+frontBack("ab") → "ba"
+*/
+/* question says that you have to do the exchange of the letters means last to first and first to last and mid is mid */
+
+
+public String frontback(string str) {
+    if (str.length() <=1)return str;
+    string mid = str.substring(1, str.length()-1);
+    return str.charAt(str.length()-1) + mid +str.charAt(0);
+}
